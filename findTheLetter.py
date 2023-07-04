c2464e39b698f06573ade19b5945cc0eed68f51c
Kadir Gülec <devguelec@gmail.com>
2023-07-04 15:00:44 +0200

    some bug fixes lol

--- findTheLetter.py
+++ findTheLetter.py
@@ -1,8 +1,18 @@
-word = input('Enter a word:')
-letter = input ('Enter a letter to search in the word:')
-num = 0
-for let in word:
-    if let == letter:
-        num +=1
-    
-print(f'there is {num} "{letter}" in {word}')
+while True:
+    print("write done to end the program")
+    word = input('Enter a word:')
+    if word == "done":
+        break
+    while True:
+        letter = input ('Enter a letter to search in the word:')
+        if len(letter) > 1:
+            print("please write just one letter")
+            continue
+        else:
+            break
+    num = 0
+    for let in word:
+        if let == letter:
+            num +=1
+        
+    print(f'there is {num} "{letter}" in {word}')
