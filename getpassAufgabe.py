a3dc9e308bcbaf14d679c1b102ab3b40e764dfa7
Kadir Gülec <devguelec@gmail.com>
2023-07-07 13:31:17 +0200

    i need to read and understand that code
    its actually not that hard to understand :)

--- getpassAufgabe.py
+++ getpassAufgabe.py
@@ -1,4 +1,33 @@
-from getpass import getpass
+""" from getpass import getpass
 
 passwort = getpass("your Passwort: ")
 print(passwort)
+ """
+from getpass import getpass
+from hashlib import pbkdf2_hmac
+
+small_letters = list('abcdefghijklmnopqrstuvwxyz')
+big_letters = list('ABCDEFGHJKLMNPQRTUVWXYZ')
+numbers = list('0123456789')
+special_characters = list('#!"§$%&/()[]{}=-_+*<>;:.')
+password_characters = small_letters + big_letters + numbers + special_characters
+salt = "pepper"
+print("*** Password Safe mit Python 3 ***")
+
+
+def convert_bytes_to_password(hashed_bytes, length):
+    number = int.from_bytes(hashed_bytes, byteorder='big')
+    password = ''
+    while number > 0 and len(password) < length:
+        password = password + password_characters[number % len(password_characters)]
+        number = number // len(password_characters)
+    return password
+
+master_password = getpass('Masterpasswort: ')
+domain = input('Domain: ')
+while len(domain) < 1:
+    print('Bitte gib eine Domain an, für die das Passwort generiert werden soll.')
+    domain = input('Domain: ')
+hash_string = domain + master_password
+hashed_bytes = pbkdf2_hmac('sha512', hash_string.encode('utf-8'), salt.encode('utf-8'), 4096)
+print('Passwort: ' + convert_bytes_to_password(hashed_bytes, 10))
